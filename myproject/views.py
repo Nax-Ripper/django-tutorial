6e69f1312051b16afdafc70bbb83d9c7507a0645
root <root@Nax-PC>
2024-10-15 22:24:04 +0800

    lesson02

--- myproject/views.py
+++ myproject/views.py
@@ -1,13 +1,14 @@
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, "home.html.j2")
+    
+    return render(request, "home.html")
     # return HttpResponse("Hello World! I'm Home")
 
 
 def about(request):
-    return render(request, "about.html.j2")
+    return render(request, "about.html")
 
     # return HttpResponse("My About page")
